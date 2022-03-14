38f04a6dcdaae46210776a75d04a6264e504d846
i-m-abbhay <20je0018@me.iitism.ac.in>
2022-03-14 14:57:16 +0530

    server return csv

--- web_application/server/app.py
+++ web_application/server/app.py
@@ -1,15 +1,11 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
 from flask_cors import CORS
 from flask_cors import cross_origin
-from werkzeug.utils import secure_filename
-from os.path import join, dirname, realpath
-from helpers import allow_file
+from helpers import allow_file, save_file
 from ml_model import curve_fit, curve_param
 
-UPLOAD_FOLDER = join(dirname(realpath(__file__)),
-                     'static/uploaded_xms_files')
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 CORS(app)
 
@@ -17,7 +13,6 @@ CORS(app)
 @app.route("/", methods=['POST'])
 @cross_origin()
 def members():
-    print(UPLOAD_FOLDER)
     if(request.method == 'POST'):
         if 'file' not in request.files:
             return "File is not selected", 400
@@ -25,14 +20,16 @@ def members():
         if(file.filename == ''):
             return "No file Selected", 400
         if file and allow_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(join(
-                app.config['UPLOAD_FOLDER'], "xms_curve." + filename.rsplit('.', 1)[1].lower()))
-            filepath = join(
-                app.config['UPLOAD_FOLDER'], "xms_curve." + filename.rsplit('.', 1)[1].lower())
-            curve_param(filepath)
-            curve_fit(filepath)
-            return "SAVED SUCCESSFULY", 200
+            filepath = save_file(file)
+            # curve_param(filepath)
+            csv_fitted_path = curve_fit(filepath)
+            with open(csv_fitted_path) as fp:
+                csv = fp.read()
+            return Response(
+                csv,
+                mimetype="text/csv",
+                headers={"Content-disposition":
+                         "attachment; filename=myplot.csv"}), 200
 
 
 if __name__ == '__main__':
--- web_application/server/helpers.py
+++ web_application/server/helpers.py
@@ -1,6 +1,23 @@
+from os.path import join, dirname, realpath
+from werkzeug.utils import secure_filename
+
+
 ALLOWED_EXTENSIONS = {'txt', 'lc', 'dat', 'xls', 'fits', 'csv'}
+
+
+XSM_UPLOAD_FOLDER = join(dirname(realpath(__file__)),
+                         'static/temp_xsm')
 
 
 def allow_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def save_file(file):
+    filename = secure_filename(file.filename)
+    file.save(join(XSM_UPLOAD_FOLDER, "xsm_curve." +
+              filename.rsplit('.', 1)[1].lower()))
+    filepath = join(XSM_UPLOAD_FOLDER, "xsm_curve." +
+                    filename.rsplit('.', 1)[1].lower())
+    return filepath
